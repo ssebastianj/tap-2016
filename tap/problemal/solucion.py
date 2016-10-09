13e80a96827c7009af8fb33d5e7ab32ea91b3b33
Sebastián J. Seba <ssebastianj@gmail.com>
2016-10-09 13:53:29 -0300

    Finalizada solución a Problema L.

--- tap/problemal/solucion.py
+++ tap/problemal/solucion.py
@@ -2,37 +2,90 @@
 
 import collections
 
-Nodo = collections.namedtuple('Nodo', 'diametro tira_izq tira_der')
+Nodo = collections.namedtuple('Nodo', 'diametro nodo_izq nodo_der')
 
 
 def puede_colocar_tira(info_tiras):
     diametro_mayor, _ = info_tiras[0]
-    tira_luces = info_tiras[1:]
-    arbol_pelotitas = crear_arbol_nodos(diametro_mayor)
-    return 'S'
+    arbol_pelotitas = crear_arbol_pelotas(diametro_mayor)
+    arbol_luces = crear_arbol_luces(info_tiras[1])
+    puede_colocar = contiene_arbol(arbol_pelotitas, arbol_luces)
+    return 'S' if puede_colocar else 'N'
 
 
-def crear_arbol_nodos(diametro):
-    gen_arbol_nodos = gen_crear_arbol_nodos(diametro)
-    return next(gen_arbol_nodos, Nodo(diametro, None, None))
+def crear_arbol_pelotas(diametro):
+    gen_arbol_nodos = gen_crear_arbol_pelotas(diametro)
+    return next(gen_arbol_nodos)
 
 
-def gen_crear_arbol_nodos(diametro):
+def gen_crear_arbol_pelotas(diametro):
     nodo_izq = None
     nodo_der = None
     if diametro >= 3:
-        nodo_izq = next(gen_crear_arbol_nodos(diametro - 1))
-        nodo_der = next(gen_crear_arbol_nodos(diametro - 2))
+        nodo_izq = next(gen_crear_arbol_pelotas(diametro - 1))
+        nodo_der = next(gen_crear_arbol_pelotas(diametro - 2))
     yield Nodo(diametro, nodo_izq, nodo_der)
 
 
-def dumps_tree(nodo):
-    dumpster = []
-    if nodo.tira_izq is not None:
-        dumpster.append(dumps_tree(nodo.tira_izq))
+def crear_arbol_luces(tira):
+    if not tira:
+        return None
 
-    dumpster.append(nodo.diametro)
+    nodo_raiz = max(tira)
+    nodo_izq = None
+    nodo_der = None
 
-    if nodo.tira_der is not None:
-        dumpster.append(dumps_tree(nodo.tira_der))
-    return tuple(dumpster)
+    max_index = tira.index(nodo_raiz)
+    part_izq = tira[:max_index]
+    part_der = tira[max_index + 1:]
+
+    if part_izq and part_der:
+        calc_izq = crear_arbol_luces(part_izq)
+        calc_der = crear_arbol_luces(part_der)
+        if part_izq[-1] > part_der[0]:
+            nodo_izq, nodo_der = calc_izq, calc_der
+        else:
+            nodo_izq, nodo_der = calc_der, calc_izq
+    elif part_izq:
+        nodo = crear_arbol_luces(part_izq)
+        if nodo_raiz - 1 == part_izq[-1]:
+            nodo_izq = nodo
+        else:
+            nodo_der = nodo
+    elif part_der:
+        nodo = crear_arbol_luces(part_der)
+        if nodo_raiz - 2 == part_der[0]:
+            nodo_der = nodo
+        else:
+            nodo_izq = nodo
+
+    return Nodo(nodo_raiz, nodo_izq, nodo_der)
+
+
+def contiene_arbol(nodo_pelota, nodo_luz):
+    if nodo_luz is None:
+        return True
+    return subarbol(nodo_pelota, nodo_luz)
+
+
+def subarbol(nodo_pelota, nodo_luz):
+    if nodo_pelota is None:
+        return False
+    elif (nodo_pelota.diametro == nodo_luz.diametro and
+          matchtree(nodo_pelota, nodo_luz)):
+        return True
+
+    return (subarbol(nodo_pelota.nodo_izq, nodo_luz) or
+            subarbol(nodo_pelota.nodo_der, nodo_luz))
+
+
+def matchtree(nodo_pelota, nodo_luz):
+    if nodo_pelota is None and nodo_luz is None:
+        return True
+    elif nodo_pelota is None or nodo_luz is None:
+        return True
+    elif nodo_pelota.diametro != nodo_luz.diametro:
+        return False
+    else:
+        return (matchtree(nodo_pelota.nodo_izq, nodo_luz.nodo_izq) and
+                matchtree(nodo_pelota.nodo_der, nodo_luz.nodo_der))
--- tests/test_problemal.py
+++ tests/test_problemal.py
@@ -3,27 +3,72 @@
 from __future__ import absolute_import
 
 from tap.problemal.solucion import (
-    puede_colocar_tira,
-    crear_arbol_nodos
+    crear_arbol_pelotas,
+    crear_arbol_luces,
+    puede_colocar_tira
 )
 
 
 class TestProblemaL(object):
     def test_crear_arbol_pelotitas_diametro_4(self):
-        nodo_raiz = crear_arbol_nodos(4)
+        nodo_raiz = crear_arbol_pelotas(4)
         assert nodo_raiz.diametro == 4
 
-        nodo_izq_1 = nodo_raiz.tira_izq
-        nodo_der_1 = nodo_raiz.tira_der
+        nodo_izq_1 = nodo_raiz.nodo_izq
+        nodo_der_1 = nodo_raiz.nodo_der
         assert nodo_izq_1.diametro == 3
         assert nodo_der_1.diametro == 2
 
-        nodo_izq_2 = nodo_izq_1.tira_izq
-        nodo_der_2 = nodo_izq_1.tira_der
+        nodo_izq_2 = nodo_izq_1.nodo_izq
+        nodo_der_2 = nodo_izq_1.nodo_der
         assert nodo_izq_2.diametro == 2
         assert nodo_der_2.diametro == 1
 
+    def test_crear_arbol_luces_1(self):
+        tira_luces = (1, 3, 4, 2)
+
+        nodo_raiz = crear_arbol_luces(tira_luces)
+        assert nodo_raiz.diametro == 4
+
+        nodo_izq_1 = nodo_raiz.nodo_izq
+        nodo_der_1 = nodo_raiz.nodo_der
+        assert nodo_izq_1.diametro == 3
+        assert nodo_der_1.diametro == 2
+
+        nodo_der_2 = nodo_izq_1.nodo_der
+        assert nodo_der_2.diametro == 1
+
+    def test_crear_arbol_luces_2(self):
+        tira_luces = (5,)
+
+        nodo_raiz = crear_arbol_luces(tira_luces)
+        assert nodo_raiz.diametro == 5
+        assert nodo_raiz.nodo_izq is None
+        assert nodo_raiz.nodo_der is None
+
+    def test_crear_arbol_luces_3(self):
+        tira_luces = (3, 5)
+
+        nodo_raiz = crear_arbol_luces(tira_luces)
+        assert nodo_raiz.diametro == 5
+
+        nodo_der = nodo_raiz.nodo_der
+        assert nodo_der.diametro == 3
+
+    def test_crear_arbol_luces_4(self):
+        tira_luces = (5, 4)
+
+        nodo_raiz = crear_arbol_luces(tira_luces)
+        assert nodo_raiz.diametro == 5
+
+        nodo_izq = nodo_raiz.nodo_izq
+        assert nodo_izq.diametro == 4
+
     def test_puede_colocar_tira(self):
+        assert puede_colocar_tira(
+            [(3, 0), tuple()]
+        ) == 'S'
+
         assert puede_colocar_tira(
             [(3, 2), (2, 3)]
         ) == 'S'
